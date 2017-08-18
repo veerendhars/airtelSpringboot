0fe3729227e6a1cb841fb6e4c54b873418146546
diksha3 <diksha3@diksha3-ThinkCentre-M710q>
2017-08-18 17:17:26 +0530

    login project

---
+++ src/main/java/com/example/main/service/UserService.java
@@ -0,0 +1,9 @@
+package com.example.main.service;
+
+import com.example.main.model.User;
+
+public interface UserService {
+	public User findUserByEmail(String email);
+
+	public void saveUser(User user);
+}
